20eb727f0d88bf30f45347141a8de2b8bb6e463b
salmonshoe <rafaelreynoso273@gmail.com>
2019-06-08 23:06:30 -0400

    Added project images and links to work.html. Modified information on the other pages to stay up-to-date

--- js/main.js
+++ js/main.js
@@ -34,3 +34,26 @@ function toggleMenu() {
 
     }
 }
+
+// Get the modal element, modal button and close button
+var modal = document.getElementById('simpleModal');
+var modalBtn = document.getElementById('modalBtn');
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+
+function openModal() {
+    modal.style.display = 'block';
+}
+
+function closeModal() {
+    modal.style.display = 'none';
+}
+
+function outsideClick() {
+    if(event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
